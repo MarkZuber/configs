8522c11ea394340c1f75bacb30814007b5956d81
Mark Zuber <mark@zube.com>
2020-07-05 21:54:11 -0700

    configure alacritty, add awesome

--- content/scripts/config_enlist.sh
+++ content/scripts/config_enlist.sh
@@ -2,9 +2,9 @@
 
 git_pull_configs() {
     # Initial repo pull and setup
-    if [ ! -d "~/.config/.git" ]; then
+    if [ ! -d "$HOME/.config/.git" ]; then
         echo Pulling git configs into ~/.config...
-        pushd ~/.config
+        pushd $HOME/.config
         git init
         git remote add origin https://github.com/markzuber/configs
         git fetch origin
@@ -17,19 +17,19 @@ git_pull_configs() {
 
 configure_symlinks() {
     # Symlinks for config files
-    if [ -f "~/.editorconfig" ]; then
+    if [ -f "$HOME/.editorconfig" ]; then
         mv ~/.editorconfig ~/.editorconfig_original
     fi
-    if [ -f "~/.gitconfig" ]; then
+    if [ -f "$HOME/.gitconfig" ]; then
         mv ~/.gitconfig ~/.gitconfig_original
     fi
-    if [ -f "~/.zshrc" ]; then
+    if [ -f "$HOME/.zshrc" ]; then
         mv ~/.zshrc ~/.zshrc_original
     fi
-    if [ -f "~/.bashrc" ]; then
+    if [ -f "$HOME/.bashrc" ]; then
         mv ~/.bashrc ~/.bashrc_original
     fi
-    if [ -f "~/.vimrc" ]; then
+    if [ -f "$HOME/.vimrc" ]; then
         mv ~/.vimrc ~/.vimrc_original
     fi
 
@@ -45,12 +45,13 @@ configure_symlinks() {
 configure_arch() {
     echo "This appears to be an Arch based system.  Configuring..."
 
-    sudo pacman --noconfirm -S yay base-devel
+    sudo pacman --noconfirm --needed -S yay base-devel
 
     arch_pkgs=(
         'neovim',
         'cowsay',
         'figlet',
+        'lolcat',
         'fortune-mod',
         'qbittorrent',
         'neofetch',
@@ -65,7 +66,8 @@ configure_arch() {
         'zsh-autosuggestions',
         'zsh-syntax-highlighting')
 
-    sudo pacman --noconfirm -S "${arch_pkgs[@]/#/-}"
+    arch_pkg_args="${arch_pkgs[@]/,/ }"
+    sudo pacman --noconfirm --needed -S $arch_pkg_args
 
     yay_pkgs=(
         'visual-studio-code-bin',
@@ -78,7 +80,8 @@ configure_arch() {
         'ttf-windows',
         'dotnet-sdk-bin')
 
-    sudo yay --noconfirm -S "${yay_pkgs[@]/#/-}"
+    yay_pkg_args="${yay_pkgs[@]/,/ }"
+    yay --noconfirm --norebuild --noredownload --needed -S $yay_pkg_args
 }
 
 configure_ubuntu() {
@@ -89,6 +92,10 @@ configure_rust() {
     rustup default stable
     cargo install exa
     cargo install cross
+}
+
+configure_fonts() {
+  ~/.config/content/fonts/install.sh
 }
 
 git_pull_configs
@@ -102,5 +109,8 @@ fi
 if [ -f "/usr/bin/apt" ]; then
     configure_ubuntu
 fi
+
+configure_rust
+configure_fonts
 
 exit $?
